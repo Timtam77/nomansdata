17f94c0828ce79026d8fb9374b5efc15c53f5694
JohnnyGloom <jcairns@cnm.edu>
2016-12-20 18:15:20 -0700

    Added a banner(temp) and some transitions

--- index.php
+++ index.php
@@ -3,21 +3,36 @@
 <html>
 
 	<head>
-
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 
 	<body>
 		
-		<nav>
-		
-		</nav>
-		
+		<header>
+		 <h1>NO MAN'S DATA</h1>
+		</header>
 		<main>
 		
+		<div id="resources">
+			<?php 
+			
+			$directory = 'icons';
+			
+			$iconImages = array_diff(scandir($directory), array('..', '.'));
+			
+			foreach($iconImages as $image){ ?>
+				
+				<div class="resource col-sm-1 col-xs-3">
+					<img src="icons/<?php echo $image ?>">
+				</div>
+			<?php } ?>
+		</div>
 		</main>
 		
 		<footer>
-		
+			<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		</footer>
 	</body>
 
